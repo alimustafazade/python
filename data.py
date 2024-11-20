ebeb3ccd18d711a851598861638aa268e65e3072
Ali Mustafayev <elimustafayev5@gmail.com>
2024-11-20 21:34:46 +0400

    first

--- data.py
+++ data.py
@@ -1,1 +1,19 @@
-print("hello world")
+# str() stringdir text= "salam" herfler 
+#int() integrer number1 = 5 reqemler
+#float() float float number = 1.5 kesr eded 
+#bool () bool bool_number = false
+
+number1 = 6 
+text = "salam"
+float_number = 1.5
+bool_number = True
+# print("55")
+# print(type(text))
+# print(type(number1))
+a = 15
+b = 10
+print(a==b)
+# 1 =lik menimsetmek 2==lik true false verir != beraber deyil 
+print(a!=b)
+name = alii
+print(name[1])
