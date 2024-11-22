e20cd4697a5b4a734ad337db19f785be97f99a9a
Ali Mustafayev <elimustafayev5@gmail.com>
2024-11-22 16:05:15 +0400

    ders2

---
+++ ders2.py
@@ -0,0 +1,41 @@
+#if elif else
+# in not in 
+#or and 
+
+
+#text = "python"
+
+#if "a" in text:
+   # print("sert odenir ")
+#else:
+ #   print("sert odenmir")
+
+# text = "java"
+# if "n" not in text:
+#     print("sert odenir ")
+# else:
+#     print("sert odenmir")
+
+# text = "javascript"
+# if "a" in text or "b" in text: # or 1 denede sert odenirse eger deyecek ki sert odenir ama and ise hamisi odenmelidir 
+#        print("sert odenir ")
+# else:
+#        print("sert odenmir")
+
+# text = "javascript"
+# if "a" in text and "s" in text:
+#     print("sert odenir ") # andde verilenin hamisi duz olmalidir ki sert odensin  yeni if-in serti odenir
+# else:
+#     print("sert odenmir")
+
+
+# number1 = 10
+# number2 = 20
+# print(max(number1, number2))
+# print(min(number1, number2)) 
+
+
+
+
+# print(pow (5, 5)) 
+
---
+++ ders3.py
@@ -0,0 +1,37 @@
+
+
+
+# my_list = ["python","php","c++", "java"]
+
+# print(my_list[2])
+
+# print(type(my_list))
+
+
+
+
+# my_list = ["python","php","c++", "java"]
+# my_list.append("c#")
+
+# my_list.remove("java")
+
+
+
+# my_list.insert(2, ("java"))  #indexe gore elave etmek 
+
+
+
+# my_list.reserve # tersine cevirir 
+
+
+# my_list.pop(3) #default olaraq sonuncunu silir , eger index yazsan indexse gore silir 
+
+
+
+
+my_list = ["python","php","c++", "java"]
+
+# istifadeci input alsin 1i basanda silsin 2 basanda elave elesin 1i basanda silmek istediyin seyin adini yaz 2 basanda elave ele ve onu gostersin 
+
+
+user = input
